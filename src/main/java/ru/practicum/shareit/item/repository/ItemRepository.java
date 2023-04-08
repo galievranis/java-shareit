5087a6f3978ef8a93aafb2689504de7b96136a60
galievranis <galiev.ranisque@gmail.com>
2023-04-08 15:23:01 +0600

    squash! style: исправить ошибки checkstyle

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -7,9 +7,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository {
+
     Item create(Item item);
+
     Item update(Long itemId, ItemDto itemDto);
+
     List<Item> getAll(Long userId);
+
     Optional<Item> getById(Long itemId);
+
     List<Item> searchItem(String searchCriteria);
 }
